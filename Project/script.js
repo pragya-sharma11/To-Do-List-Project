1a58b919380991863b1b1c616866f081a5b7b405
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-03 20:10:26 +0530

    finally code get shorten

--- Project/script.js
+++ Project/script.js
@@ -4,22 +4,21 @@ let btnAdd = $('#btnAdd')
 let btnClear = $('#btnClear')
 let inpNewTask  = $('#inpNewTask')
 function addItem(){
-        let listitem = $('<li>', {
-            'class':'list-group-item',
-            text:inpNewTask.val()
-        })
-        ulTasks.append(listitem)
-        inpNewTask.val('')
-        listitem.click((event) => {
-            //console.log("clicked", $(event.target))--->just for debugging!!
-            $(event.target).toggleClass('done')
-            //toggleClass()-->changes the class of any element
-        
-})
+    let listitem = $('<li>', {
+        'class':'list-group-item',
+        text:inpNewTask.val()
+    })
+    ulTasks.append(listitem)
+    inpNewTask.val('')
+    listitem.click((event) => {
+        //console.log("clicked", $(event.target))--->just for debugging!!
+        $(event.target).toggleClass('done')
+        //toggleClass()-->changes the class of any element
+    })
 }
 inpNewTask.keypress((e) =>{
     if(e.which==13){
-        addItem()
+       addItem()
     }
     //console.log(e.which)
     /**
@@ -31,22 +30,13 @@ inpNewTask.keypress((e) =>{
      * alternate way is :--
      * e.which --->gives keycode
      */
-    //console.log(e.which)
-    /**
-     * for debugging
-     * go into the output on console
-     * search for original event
-     * then inside it, search for keycode
-     * and we get the keycode
-     * alternate way is :--
-     * e.which --->gives keycode
-     */
 })
-btnAdd.click(addItem())
+btnAdd.click(()=>{
+    addItem()
+})
 
 btnClear.click(()=>{
     inpNewTask.val("")
 })
-
 })
 
