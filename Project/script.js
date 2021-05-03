8b79dbc1c11ca60cd4c954365ee671d1548b4a6f
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-03 18:27:10 +0530

    add the input item to list

--- Project/script.js
+++ Project/script.js
@@ -5,6 +5,10 @@ let btnClear = $('#btnClear')
 let inpNewTask  = $('#inpNewTask')
 
 $('#btnAdd').click(()=>{
+    let listitem = $('<li>', {
+        'class':'list-group-item',
+        text:inpNewTask.val()
+    })
     console.log(inpNewTask.val())
     inpNewTask.val("")
 })
